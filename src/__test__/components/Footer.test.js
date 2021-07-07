9f0a689f981327ddf08a3113fe23d58e25284b86
Visaka Devi <visaka.chaves@consorcio.cl>
2021-07-06 21:10:23 -0400

    docs: test actions

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { create } from 'react-test-renderer';
 import Footer from '../../components/Footer';
 
 describe('<Footer/> Elemento Funciona', () => {
+
+  const footer = mount(<Footer />);
   test('Render del componente Footer', () => {
-    const footer = mount(<Footer />);
     expect(footer.length).toEqual(1);
   });
+  test('Render del titulo', () => {
+    expect(footer.find('.Footer-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('<Footer/> SnapShot', () => {
+
+  test('Comprobar las ui del componente Footer', () => {
+    const footer = create(<Footer />);
+    expect(footer.toJSON()).toMatchSnapshot();
+
+  });
+
 });
---
+++ src/__test__/components/Header.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
+import Header from '../../components/Header';
+import ProviderMock from '../../__mocks__/providerMock';
+
+describe('<Header/> Elemento Funciona', () => {
+  test('Render del componente Header', () => {
+    const header = shallow(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.length).toEqual(1);
+  });
+  test('Render del titulo', () => {
+    const header = mount(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header SnapShot', () => {
+  test('comprobar el snap', () => {
+    const header = create(
+      <ProviderMock>
+        <Header />
+      </ProviderMock>
+    );
+    expect(header.toJSON()).toMatchSnapshot();
+  });
+});
